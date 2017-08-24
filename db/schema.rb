0000d51a2d3f156f8d822517a2200a12a9f1bbf2
Chris Nyaga <nyagachris75@gmail.com>
2017-08-24 12:22:25 +0300

    Migration to add name to review and resulting schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824090325) do
+ActiveRecord::Schema.define(version: 20170824092048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170824090325) do
     t.integer "rating", default: 0
     t.string "city"
     t.string "country"
+    t.string "name"
   end
 
 end
