90ec1e6b7b1d5e5d5a6c0efe60a011e8656a7f82
Chris Nyaga <nyagachris75@gmail.com>
2017-08-24 13:53:27 +0300

    Added class methods to allow a user find a review by specifying the country or city.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
   validates :name, :content, :author_full_name, :author_user_name, :rating, :city, :country, presence: true
+  def self.country_search(country)
+    # Search using country and downcase so as to find lowercase attributes also.
+    Review.where("lower(country) like ?", "#{country.downcase}")
+  end
+
+  def self.city_search(city)
+    # Search using city and downcase so as to find lowercase attributes also.
+    Review.where("lower(city) like ?", "#{city.downcase}")
+  end
 end
