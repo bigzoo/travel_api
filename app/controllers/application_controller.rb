24c09e44ab4c02ee124fd940a938a761a20b0582
Chris Nyaga <nyagachris75@gmail.com>
2017-08-24 12:24:54 +0300

    Rescue methods for incorrect api calls added to the application controller and Response controller module used to parse json responses.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  include Response
+  rescue_from ActionController::RoutingError do |exception|
+    json_response({ error: exception.message }, :bad_request)
+  end
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_response({ error: exception.message }, :not_found)
+  end
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_response({ error: exception.message }, :unprocessable_entity)
+  end
 end
